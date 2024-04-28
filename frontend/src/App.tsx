1ab0dab4535fd53688a1587c890e1f9ea457e2fb
Divyansh Jain <divyanshrajeshjain@gmail.com>
2024-04-27 22:10:13 -0700

    Bootstrap Working

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
+import Login from './pages/login';
+
+import "bootstrap";
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
 function App() {
+  return (
+    <div className="mt-5 px-3 py-4">
+      <h1 className="h1 display-1">Counter Component</h1>
+      <h2 className="h1 display-1 mt-5 mb-3">0</h2>
+      <div className="mt-5">
+        <button
+          className="btn btn-primary btn-lg mx-3 px-5 py-3 mt-2">
+          Add
+        </button>
+        <button
+          className="btn btn-dark btn-lg mx-3 px-5 py-3 mt-2" >
+          Remove
+        </button>
+      </div>
+    </div>
+  )
+  /*
   return (
     <div className="App">
       <header className="App-header">
@@ -21,6 +42,7 @@ function App() {
       </header>
     </div>
   );
+  */
 }
 
 export default App;
