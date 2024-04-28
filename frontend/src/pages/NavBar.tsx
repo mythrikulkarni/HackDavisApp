e16f9c5480d5fa45b0eaf95c4ad0256f6ca58bf8
diegoo <drafael038@insite.4cd.edu>
2024-04-28 02:56:15 -0700

    NavBar added

---
+++ frontend/src/pages/NavBar.tsx
@@ -0,0 +1,194 @@
+import React from 'react' 
+
+function NavBar() {
+    return (
+            <div
+    className="BotNav"
+    style={{
+        width: 390,
+        height: 75,
+        left: 0,
+        top: 754,
+        position: "relative",
+        background: "#EAEAEA",
+    }}
+    >
+    <div
+        className="Frame133"
+        style={{
+        width: 66,
+        padding: 5,
+        left: 35,
+        top: 4,
+        position: "absolute",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: 10,
+        display: "inline-flex",
+        }}
+    >
+        <div
+        className="Component5"
+        style={{ width: 56, height: 51, position: "relative" }}
+        >
+        <div
+            className="SolidCommunicationComment"
+            style={{
+            width: 32,
+            height: 32,
+            left: 13,
+            top: 0,
+            position: "absolute",
+            }}
+        >
+            <svg
+            width="24"
+            height="23"
+            viewBox="0 0 24 23"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            >
+            <path
+                d="M12.4812 0.333374C4.77421 0.333374 -0.476648 8.14251 2.43119 15.2799L3.67431 18.3312C3.78554 18.6042 3.70423 18.9179 3.47435 19.1025L0.845698 21.2136C0.625126 21.3907 0.54037 21.6878 0.634262 21.9546C0.728153 22.2215 0.980244 22.4 1.26314 22.4H11.6902C18.1205 22.4 23.3333 17.1872 23.3333 10.7569C23.3333 5.00015 18.6665 0.333374 12.9098 0.333374H12.4812Z"
+                fill="black"
+            />
+            </svg>
+        </div>
+        <div
+            className="Messages"
+            style={{
+            left: 0,
+            top: 37,
+            position: "absolute",
+            color: "black",
+            fontSize: 12,
+            fontFamily: "Roboto",
+            fontWeight: "400",
+            wordWrap: "break-word",
+            }}
+        >
+            Messages
+        </div>
+        </div>
+    </div>
+    <div
+        className="Frame134"
+        style={{
+        width: 59,
+        padding: 5,
+        left: 171,
+        top: 4,
+        position: "absolute",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: 10,
+        display: "inline-flex",
+        }}
+    >
+        <div
+        className="Component6"
+        style={{ width: 49, height: 51, position: "relative" }}
+        >
+        <div
+            className="SolidStatusBookOpen"
+            style={{ width: 32, height: 32, left: 9, top: 0, position: "absolute" }}
+        >
+            <svg
+            width="28"
+            height="26"
+            viewBox="0 0 28 26"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            >
+            <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M13.3198 2.48108C9.58552 0.24052 5.0185 -0.125423 0.975106 1.49194C0.386176 1.72751 0 2.2979 0 2.9322V18.4774C0 19.5187 1.05168 20.2307 2.01849 19.844C5.32961 18.5195 9.06953 18.8192 12.1275 20.654L13.6412 21.5622C13.7567 21.6314 13.8801 21.6617 14 21.6596C14.1199 21.6617 14.2434 21.6314 14.3588 21.5622L15.8725 20.654C18.9305 18.8192 22.6704 18.5195 25.9815 19.844C26.9483 20.2307 28 19.5187 28 18.4774V2.9322C28 2.2979 27.6138 1.72751 27.0249 1.49194C22.9815 -0.125423 18.4145 0.24052 14.6802 2.48108L14 2.88921L13.3198 2.48108ZM15 5.66667C15 5.11438 14.5523 4.66667 14 4.66667C13.4477 4.66667 13 5.11438 13 5.66667V18.3333C13 18.8856 13.4477 19.3333 14 19.3333C14.5523 19.3333 15 18.8856 15 18.3333V5.66667Z"
+                fill="black"
+            />
+            <path
+                d="M1.63312 22.3896C4.33159 20.8155 7.6684 20.8155 10.3669 22.3896L11.8166 23.2353C13.1658 24.0223 14.8342 24.0223 16.1834 23.2353L17.6331 22.3896C20.3316 20.8155 23.6684 20.8155 26.3669 22.3896L26.5039 22.4696C26.9809 22.7478 27.1421 23.3602 26.8638 23.8372C26.5855 24.3143 25.9732 24.4754 25.4961 24.1971L25.3591 24.1172C23.2834 22.9064 20.7166 22.9064 18.6409 24.1172L17.1912 24.9629C15.2192 26.1132 12.7808 26.1132 10.8088 24.9629L9.35914 24.1172C7.28339 22.9064 4.71661 22.9064 2.64086 24.1172L2.50387 24.1971C2.02682 24.4754 1.4145 24.3143 1.13622 23.8372C0.857941 23.3602 1.01908 22.7478 1.49613 22.4696L1.63312 22.3896Z"
+                fill="black"
+            />
+            </svg>
+        </div>
+        <div
+            className="Contacts"
+            style={{
+            left: 0,
+            top: 37,
+            position: "absolute",
+            color: "black",
+            fontSize: 12,
+            fontFamily: "Roboto",
+            fontWeight: "400",
+            wordWrap: "break-word",
+            }}
+        >
+            Contacts
+        </div>
+        </div>
+    </div>
+    <div
+        className="Frame135"
+        style={{
+        width: 45,
+        padding: 5,
+        left: 310,
+        top: 4,
+        position: "absolute",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: 10,
+        display: "inline-flex",
+        }}
+    >
+        <div
+        className="Component7"
+        style={{ width: 35, height: 51, position: "relative" }}
+        >
+        <div
+            className="SolidCommunicationUser"
+            style={{ width: 32, height: 32, left: 2, top: 0, position: "absolute" }}
+        >
+            <svg
+            width="22"
+            height="22"
+            viewBox="0 0 22 22"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            >
+            <path
+                d="M11 0C8.23854 0 5.99996 2.23858 5.99996 5C5.99996 7.76142 8.23854 10 11 10C13.7614 10 16 7.76142 16 5C16 2.23858 13.7614 0 11 0Z"
+                fill="black"
+            />
+            <path
+                d="M5.66663 12.6667C2.9052 12.6667 0.666626 14.9052 0.666626 17.6667V19.251C0.666626 20.2554 1.39447 21.1117 2.38565 21.2735C8.09076 22.2049 13.9092 22.2049 19.6143 21.2735C20.6054 21.1117 21.3333 20.2554 21.3333 19.251V17.6667C21.3333 14.9052 19.0947 12.6667 16.3333 12.6667H15.8788C15.6328 12.6667 15.3884 12.7056 15.1545 12.7819L14.0005 13.1587C12.0508 13.7954 9.94912 13.7954 7.99941 13.1587L6.84539 12.7819C6.61155 12.7056 6.36711 12.6667 6.12112 12.6667H5.66663Z"
+                fill="black"
+            />
+            </svg>
+        </div>
+        <div
+            className="Profile"
+            style={{ left: 0, top: 37, position: "absolute" }}
+        >
+            <span
+            style={{
+                color: "black",
+                fontSize: 12,
+                fontFamily: "Roboto",
+                fontWeight: "400",
+                wordWrap: "break-word",
+            }}
+            >
+            Profile
+            </span>
+        </div>
+        </div>
+    </div>
+    </div>
+    )
+}
+
+export default NavBar
