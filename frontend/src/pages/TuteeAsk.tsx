7b51684bbeadf3d913888db1085d0a9fd91cf483
diegoo <drafael038@insite.4cd.edu>
2024-04-28 02:31:01 -0700

    TuteeAsk and TuteeTopics added

---
+++ frontend/src/pages/TuteeAsk.tsx
@@ -0,0 +1,376 @@
+import React from 'react';
+
+function TutteeAsk() {
+    return (
+            <div
+    className="TuteeHomePage container"
+    style={{ width: 390, height: 844, position: "relative", background: "white" }}
+    >
+    <div
+        className="BotNav"
+        style={{
+        width: 390,
+        height: 90,
+        left: 0,
+        top: 754,
+        position: "absolute",
+        background: "#EAEAEA",
+        }}
+    >
+        <div
+        className="HomeIndicator"
+        style={{ width: 390, height: 20, left: 0, top: 70, position: "absolute" }}
+        >
+        <div
+            className="HomeIndicator"
+            style={{
+            width: 134,
+            height: 5,
+            left: 128,
+            top: 7,
+            position: "absolute",
+            background: "black",
+            borderRadius: 100,
+            }}
+        />
+        </div>
+        <div
+        className="Frame133"
+        style={{
+            width: 66,
+            padding: 5,
+            left: 35,
+            top: 4,
+            position: "absolute",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: 10,
+            display: "inline-flex",
+        }}
+        >
+        <div
+            className="Component5"
+            style={{ width: 56, height: 51, position: "relative" }}
+        >
+            <div
+            className="SolidCommunicationComment"
+            style={{
+                width: 32,
+                height: 32,
+                left: 13,
+                top: 0,
+                position: "absolute",
+            }}
+            >
+            <div
+                className="Icon"
+                style={{
+                width: 22.74,
+                height: 22.07,
+                left: 4.6,
+                top: 5.33,
+                position: "absolute",
+                background: "black",
+                }}
+            ></div>
+            </div>
+            <div
+            className="Messages"
+            style={{
+                left: 0,
+                top: 37,
+                position: "absolute",
+                color: "black",
+                fontSize: 12,
+                fontFamily: "Roboto",
+                fontWeight: "400",
+                wordWrap: "break-word",
+            }}
+            >
+            Messages
+            </div>
+        </div>
+        </div>
+        <div
+        className="Frame134"
+        style={{
+            width: 59,
+            padding: 5,
+            left: 171,
+            top: 4,
+            position: "absolute",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: 10,
+            display: "inline-flex",
+        }}
+        >
+        <div
+            className="Component6"
+            style={{ width: 49, height: 51, position: "relative" }}
+        >
+            <div
+            className="SolidStatusBookOpen"
+            style={{
+                width: 32,
+                height: 32,
+                left: 9,
+                top: 0,
+                position: "absolute",
+            }}
+            >
+            <div
+                className="Icon"
+                style={{
+                width: 28,
+                height: 25.33,
+                left: 2,
+                top: 3.5,
+                position: "absolute",
+                background: "black",
+                }}
+            ></div>
+            </div>
+            <div
+            className="Contacts"
+            style={{
+                left: 0,
+                top: 37,
+                position: "absolute",
+                color: "black",
+                fontSize: 12,
+                fontFamily: "Roboto",
+                fontWeight: "400",
+                wordWrap: "break-word",
+            }}
+            >
+            Contacts
+            </div>
+        </div>
+        </div>
+        <div
+        className="Frame135"
+        style={{
+            width: 45,
+            padding: 5,
+            left: 310,
+            top: 4,
+            position: "absolute",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: 10,
+            display: "inline-flex",
+        }}
+        >
+        <div
+            className="Component7"
+            style={{ width: 35, height: 51, position: "relative" }}
+        >
+            <div
+            className="SolidCommunicationUser"
+            style={{
+                width: 32,
+                height: 32,
+                left: 2,
+                top: 0,
+                position: "absolute",
+            }}
+            >
+            <div
+                className="Icon"
+                style={{
+                width: 20.67,
+                height: 21.97,
+                left: 5.67,
+                top: 5,
+                position: "absolute",
+                background: "black",
+                }}
+            ></div>
+            </div>
+            <div
+            className="Profile"
+            style={{
+                left: 0,
+                top: 37,
+                position: "absolute",
+                color: "black",
+                fontSize: 12,
+                fontFamily: "Roboto",
+                fontWeight: "400",
+                wordWrap: "break-word",
+            }}
+            >
+            Profile
+            </div>
+        </div>
+        </div>
+    </div>
+    <div
+        className="Frame139"
+        style={{
+        left: 30,
+        top: 60,
+        position: "absolute",
+        justifyContent: "flex-start",
+        alignItems: "flex-start",
+        gap: 10,
+        display: "inline-flex",
+        }}
+    >
+        <div
+        className="Tutee"
+        style={{
+            color: "black",
+            fontSize: 24,
+            fontFamily: "Inter",
+            fontWeight: "700",
+            wordWrap: "break-word",
+        }}
+        >
+        Tutee
+        </div>
+        <div
+        className="Group58"
+        style={{ width: 64, height: 29, position: "relative" }}
+        >
+        <div
+            className="Frame130"
+            style={{
+            width: 64,
+            height: 29,
+            left: 0,
+            top: 0,
+            position: "absolute",
+            borderRadius: 20,
+            border: "1px #C2C2C2 solid",
+            }}
+        />
+        <div
+            className="Ellipse19"
+            style={{
+            width: 19,
+            height: 19,
+            left: 40,
+            top: 5,
+            position: "absolute",
+            background: "#DF7D36",
+            borderRadius: 9999,
+            }}
+        />
+        <div
+            className="Ellipse20"
+            style={{
+            width: 19,
+            height: 19,
+            left: 5,
+            top: 5,
+            position: "absolute",
+            borderRadius: 9999,
+            border: "1px #C2C2C2 solid",
+            }}
+        />
+        </div>
+    </div>
+    <div
+        className="AskAQuestion"
+        style={{
+        left: 30,
+        top: 129,
+        position: "absolute",
+        color: "black",
+        fontSize: 18,
+        fontFamily: "Inter",
+        fontWeight: "500",
+        wordWrap: "break-word",
+        }}
+    >
+        Ask a question
+    </div>
+    <div
+        className="Frame141"
+        style={{
+        height: 344,
+        padding: 15,
+        left: 30,
+        right: 30,
+        top: 161,
+        position: "absolute",
+        background: "#EAEAEA",
+        borderRadius: 10,
+        overflow: "hidden",
+        flexDirection: "column",
+        justifyContent: "flex-start",
+        alignItems: "flex-start",
+        gap: 276,
+        display: "inline-flex",
+        }}
+    >
+        <div
+        className="AskHere"
+        style={{
+            color: "#858585",
+            fontSize: 16,
+            fontFamily: "Inter",
+            fontWeight: "500",
+            wordWrap: "break-word",
+        }}
+        >
+        ask here ...
+        </div>
+        <div
+        className="200"
+        style={{
+            color: "#858585",
+            fontSize: 16,
+            fontFamily: "Inter",
+            fontWeight: "500",
+            wordWrap: "break-word",
+        }}
+        >
+        0/200
+        </div>
+    </div>
+    <div
+        className="Frame96"
+        style={{
+        width: 330,
+        height: 47,
+        paddingLeft: 87,
+        paddingRight: 87,
+        paddingTop: 16,
+        paddingBottom: 16,
+        left: 25,
+        top: 682,
+        position: "absolute",
+        background: "#E4E4E4",
+        borderRadius: 10,
+        overflow: "hidden",
+        border: "1px solid",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: 10,
+        display: "inline-flex",
+        }}
+    >
+        <div
+        className="ChooseTopic"
+        style={{
+            color: "black",
+            fontSize: 14,
+            fontFamily: "Inter",
+            fontWeight: "500",
+            wordWrap: "break-word",
+        }}
+        >
+        Choose Topic
+        </div>
+    </div>
+    </div>
+    
+    )
+}
+
+export default TutteeAsk
---
+++ frontend/src/pages/TuteeTopics.tsx
@@ -0,0 +1,654 @@
+import React from 'react';
+
+function TuteeTopics() {
+    return (
+            <div
+    className="TuteeHomePage container"
+    style={{ width: 390, height: 844, position: "relative", background: "white" }}
+    >
+    <div
+        className="BotNav"
+        style={{
+        width: 390,
+        height: 90,
+        left: 0,
+        top: 754,
+        position: "absolute",
+        background: "#EAEAEA",
+        }}
+    >
+        <div
+        className="HomeIndicator"
+        style={{ width: 390, height: 20, left: 0, top: 70, position: "absolute" }}
+        >
+        <div
+            className="HomeIndicator"
+            style={{
+            width: 134,
+            height: 5,
+            left: 128,
+            top: 7,
+            position: "absolute",
+            background: "black",
+            borderRadius: 100,
+            }}
+        />
+        </div>
+        <div
+        className="Frame133"
+        style={{
+            width: 66,
+            padding: 5,
+            left: 35,
+            top: 4,
+            position: "absolute",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: 10,
+            display: "inline-flex",
+        }}
+        >
+        <div
+            className="Component5"
+            style={{ width: 56, height: 51, position: "relative" }}
+        >
+            <div
+            className="SolidCommunicationComment"
+            style={{
+                width: 32,
+                height: 32,
+                left: 13,
+                top: 0,
+                position: "absolute",
+            }}
+            >
+            <div
+                className="Icon"
+                style={{
+                width: 22.74,
+                height: 22.07,
+                left: 4.6,
+                top: 5.33,
+                position: "absolute",
+                background: "black",
+                }}
+            ></div>
+            </div>
+            <div
+            className="Messages"
+            style={{
+                left: 0,
+                top: 37,
+                position: "absolute",
+                color: "black",
+                fontSize: 12,
+                fontFamily: "Roboto",
+                fontWeight: "400",
+                wordWrap: "break-word",
+            }}
+            >
+            Messages
+            </div>
+        </div>
+        </div>
+        <div
+        className="Frame134"
+        style={{
+            width: 59,
+            padding: 5,
+            left: 171,
+            top: 4,
+            position: "absolute",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: 10,
+            display: "inline-flex",
+        }}
+        >
+        <div
+            className="Component6"
+            style={{ width: 49, height: 51, position: "relative" }}
+        >
+            <div
+            className="SolidStatusBookOpen"
+            style={{
+                width: 32,
+                height: 32,
+                left: 9,
+                top: 0,
+                position: "absolute",
+            }}
+            >
+            <div
+                className="Icon"
+                style={{
+                width: 28,
+                height: 25.33,
+                left: 2,
+                top: 3.5,
+                position: "absolute",
+                background: "black",
+                }}
+            ></div>
+            </div>
+            <div
+            className="Contacts"
+            style={{
+                left: 0,
+                top: 37,
+                position: "absolute",
+                color: "black",
+                fontSize: 12,
+                fontFamily: "Roboto",
+                fontWeight: "400",
+                wordWrap: "break-word",
+            }}
+            >
+            Contacts
+            </div>
+        </div>
+        </div>
+        <div
+        className="Frame135"
+        style={{
+            width: 45,
+            padding: 5,
+            left: 310,
+            top: 4,
+            position: "absolute",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: 10,
+            display: "inline-flex",
+        }}
+        >
+        <div
+            className="Component7"
+            style={{ width: 35, height: 51, position: "relative" }}
+        >
+            <div
+            className="SolidCommunicationUser"
+            style={{
+                width: 32,
+                height: 32,
+                left: 2,
+                top: 0,
+                position: "absolute",
+            }}
+            >
+            <div
+                className="Icon"
+                style={{
+                width: 20.67,
+                height: 21.97,
+                left: 5.67,
+                top: 5,
+                position: "absolute",
+                background: "black",
+                }}
+            ></div>
+            </div>
+            <div
+            className="Profile"
+            style={{
+                left: 0,
+                top: 37,
+                position: "absolute",
+                color: "black",
+                fontSize: 12,
+                fontFamily: "Roboto",
+                fontWeight: "400",
+                wordWrap: "break-word",
+            }}
+            >
+            Profile
+            </div>
+        </div>
+        </div>
+    </div>
+    <div
+        className="ChooseTopic"
+        style={{
+        left: 30,
+        top: 129,
+        position: "absolute",
+        color: "black",
+        fontSize: 18,
+        fontFamily: "Inter",
+        fontWeight: "500",
+        wordWrap: "break-word",
+        }}
+    >
+        Choose Topic
+    </div>
+    <div
+        className="Frame139"
+        style={{
+        left: 30,
+        top: 60,
+        position: "absolute",
+        justifyContent: "flex-start",
+        alignItems: "flex-start",
+        gap: 10,
+        display: "inline-flex",
+        }}
+    >
+        <div
+        className="Tutee"
+        style={{
+            color: "black",
+            fontSize: 24,
+            fontFamily: "Inter",
+            fontWeight: "700",
+            wordWrap: "break-word",
+        }}
+        >
+        Tutee
+        </div>
+        <div
+        className="Group58"
+        style={{ width: 64, height: 29, position: "relative" }}
+        >
+        <div
+            className="Frame130"
+            style={{
+            width: 64,
+            height: 29,
+            left: 0,
+            top: 0,
+            position: "absolute",
+            borderRadius: 20,
+            border: "1px #C2C2C2 solid",
+            }}
+        />
+        <div
+            className="Ellipse19"
+            style={{
+            width: 19,
+            height: 19,
+            left: 40,
+            top: 5,
+            position: "absolute",
+            background: "#DF7D36",
+            borderRadius: 9999,
+            }}
+        />
+        <div
+            className="Ellipse20"
+            style={{
+            width: 19,
+            height: 19,
+            left: 5,
+            top: 5,
+            position: "absolute",
+            borderRadius: 9999,
+            border: "1px #C2C2C2 solid",
+            }}
+        />
+        </div>
+    </div>
+    <div
+        className="Group59"
+        style={{
+        width: 145,
+        height: 464,
+        left: 30,
+        top: 171,
+        position: "absolute",
+        }}
+    >
+        <div
+        className="Frame100"
+        style={{
+            width: 24,
+            height: 24,
+            left: 0,
+            top: 0,
+            position: "absolute",
+            borderRadius: 5,
+            border: "1px black solid",
+        }}
+        />
+        <div
+        className="Frame101"
+        style={{
+            width: 24,
+            height: 24,
+            left: 0,
+            top: 44,
+            position: "absolute",
+            borderRadius: 5,
+            border: "1px black solid",
+        }}
+        />
+        <div
+        className="Frame102"
+        style={{
+            width: 24,
+            height: 24,
+            left: 0,
+            top: 88,
+            position: "absolute",
+            borderRadius: 5,
+            border: "1px black solid",
+        }}
+        />
+        <div
+        className="Frame103"
+        style={{
+            width: 24,
+            height: 24,
+            left: 0,
+            top: 132,
+            position: "absolute",
+            borderRadius: 5,
+            border: "1px black solid",
+        }}
+        />
+        <div
+        className="Frame104"
+        style={{
+            width: 24,
+            height: 24,
+            left: 0,
+            top: 176,
+            position: "absolute",
+            borderRadius: 5,
+            border: "1px black solid",
+        }}
+        />
+        <div
+        className="Frame105"
+        style={{
+            width: 24,
+            height: 24,
+            left: 0,
+            top: 220,
+            position: "absolute",
+            borderRadius: 5,
+            border: "1px black solid",
+        }}
+        />
+        <div
+        className="Frame106"
+        style={{
+            width: 24,
+            height: 24,
+            left: 0,
+            top: 264,
+            position: "absolute",
+            borderRadius: 5,
+            border: "1px black solid",
+        }}
+        />
+        <div
+        className="Frame107"
+        style={{
+            width: 24,
+            height: 24,
+            left: 0,
+            top: 308,
+            position: "absolute",
+            borderRadius: 5,
+            border: "1px black solid",
+        }}
+        />
+        <div
+        className="Frame108"
+        style={{
+            width: 24,
+            height: 24,
+            left: 0,
+            top: 352,
+            position: "absolute",
+            borderRadius: 5,
+            border: "1px black solid",
+        }}
+        />
+        <div
+        className="Frame109"
+        style={{
+            width: 24,
+            height: 24,
+            left: 0,
+            top: 396,
+            position: "absolute",
+            borderRadius: 5,
+            border: "1px black solid",
+        }}
+        />
+        <div
+        className="Frame110"
+        style={{
+            width: 24,
+            height: 24,
+            left: 0,
+            top: 440,
+            position: "absolute",
+            borderRadius: 5,
+            border: "1px black solid",
+        }}
+        />
+        <div
+        className="Academics"
+        style={{
+            left: 38,
+            top: 1,
+            position: "absolute",
+            color: "black",
+            fontSize: 18,
+            fontFamily: "Inter",
+            fontWeight: "400",
+            wordWrap: "break-word",
+        }}
+        >
+        Academics
+        </div>
+        <div
+        className="Technology"
+        style={{
+            left: 38,
+            top: 45,
+            position: "absolute",
+            color: "black",
+            fontSize: 18,
+            fontFamily: "Inter",
+            fontWeight: "400",
+            wordWrap: "break-word",
+        }}
+        >
+        Technology
+        </div>
+        <div
+        className="Health"
+        style={{
+            left: 38,
+            top: 89,
+            position: "absolute",
+            color: "black",
+            fontSize: 18,
+            fontFamily: "Inter",
+            fontWeight: "400",
+            wordWrap: "break-word",
+        }}
+        >
+        Health
+        </div>
+        <div
+        className="Fitness"
+        style={{
+            left: 38,
+            top: 133,
+            position: "absolute",
+            color: "black",
+            fontSize: 18,
+            fontFamily: "Inter",
+            fontWeight: "400",
+            wordWrap: "break-word",
+        }}
+        >
+        Fitness
+        </div>
+        <div
+        className="Cooking"
+        style={{
+            left: 38,
+            top: 177,
+            position: "absolute",
+            color: "black",
+            fontSize: 18,
+            fontFamily: "Inter",
+            fontWeight: "400",
+            wordWrap: "break-word",
+        }}
+        >
+        Cooking
+        </div>
+        <div
+        className="Fashion"
+        style={{
+            left: 38,
+            top: 221,
+            position: "absolute",
+            color: "black",
+            fontSize: 18,
+            fontFamily: "Inter",
+            fontWeight: "400",
+            wordWrap: "break-word",
+        }}
+        >
+        Fashion
+        </div>
+        <div
+        className="Sports"
+        style={{
+            left: 38,
+            top: 265,
+            position: "absolute",
+            color: "black",
+            fontSize: 18,
+            fontFamily: "Inter",
+            fontWeight: "400",
+            wordWrap: "break-word",
+        }}
+        >
+        Sports
+        </div>
+        <div
+        className="Business"
+        style={{
+            left: 38,
+            top: 309,
+            position: "absolute",
+            color: "black",
+            fontSize: 18,
+            fontFamily: "Inter",
+            fontWeight: "400",
+            wordWrap: "break-word",
+        }}
+        >
+        Business
+        </div>
+        <div
+        className="ArtDesign"
+        style={{
+            left: 38,
+            top: 353,
+            position: "absolute",
+            color: "black",
+            fontSize: 18,
+            fontFamily: "Inter",
+            fontWeight: "400",
+            wordWrap: "break-word",
+        }}
+        >
+        Art & Design
+        </div>
+        <div
+        className="Gaming"
+        style={{
+            left: 38,
+            top: 397,
+            position: "absolute",
+            color: "black",
+            fontSize: 18,
+            fontFamily: "Inter",
+            fontWeight: "400",
+            wordWrap: "break-word",
+        }}
+        >
+        Gaming
+        </div>
+        <div
+        className="Finance"
+        style={{
+            left: 38,
+            top: 441,
+            position: "absolute",
+            color: "black",
+            fontSize: 18,
+            fontFamily: "Inter",
+            fontWeight: "400",
+            wordWrap: "break-word",
+        }}
+        >
+        Finance
+        </div>
+    </div>
+    <div
+        className="Frame96"
+        style={{
+        width: 330,
+        height: 47,
+        paddingLeft: 87,
+        paddingRight: 87,
+        paddingTop: 16,
+        paddingBottom: 16,
+        left: 30,
+        top: 682,
+        position: "absolute",
+        background: "#E4E4E4",
+        borderRadius: 10,
+        overflow: "hidden",
+        border: "1px solid",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: 10,
+        display: "inline-flex",
+        }}
+    >
+        <div
+        className="Post"
+        style={{
+            color: "black",
+            fontSize: 14,
+            fontFamily: "Inter",
+            fontWeight: "500",
+            wordWrap: "break-word",
+        }}
+        >
+        Post
+        </div>
+    </div>
+    <div
+        className="Frame142"
+        style={{
+        width: 390,
+        height: 56,
+        left: 0,
+        top: 580,
+        position: "absolute",
+        background: "white",
+        boxShadow:
+            "13.699999809265137px 13.699999809265137px 13.699999809265137px ",
+        filter: "blur(50px)",
+        }}
+    />
+    </div>
+
+    )
+}
+
+export default TuteeTopics
