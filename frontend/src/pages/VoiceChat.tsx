1502864e32c45d43d2ed018c33882a2b654f2e9a
diegoo <drafael038@insite.4cd.edu>
2024-04-28 08:39:03 -0700

    Voice Chat design implemented

---
+++ frontend/src/pages/VoiceChat.tsx
@@ -0,0 +1,280 @@
+import React from "react";
+
+function VoiceChat() {
+    return (
+            <div
+    className="VideoCall container"
+    style={{
+        width: 390,
+        height: 844,
+        position: "relative",
+        background: "#FAF9F6",
+    }}
+    >
+    <div
+        className="Frame144"
+        style={{
+        width: 330,
+        paddingTop: 80,
+        left: 30,
+        top: 60,
+        position: "absolute",
+        background: "#F6E8BD",
+        borderRadius: 10,
+        overflow: "hidden",
+        flexDirection: "column",
+        justifyContent: "flex-start",
+        alignItems: "center",
+        gap: 90,
+        display: "inline-flex",
+        }}
+    >
+        <div
+        className="Frame146"
+        style={{
+            width: 100,
+            height: 100,
+            position: "relative",
+            background: "#1A1A1A",
+            borderRadius: 8,
+            overflow: "hidden",
+        }}
+        >
+        <div
+            className="P"
+            style={{
+            left: 37,
+            top: 26,
+            position: "absolute",
+            color: "white",
+            fontSize: 40,
+            fontFamily: "Inter",
+            fontWeight: "500",
+            wordWrap: "break-word",
+            }}
+        >
+            P
+        </div>
+        </div>
+        <div
+        className="Frame151"
+        style={{
+            width: 330,
+            height: 55,
+            position: "relative",
+            background: "#E2B11D",
+            overflow: "hidden",
+        }}
+        >
+        <div
+            className="PatrickNelson"
+            style={{
+            left: 15,
+            top: 18,
+            position: "absolute",
+            color: "#1A1A1A",
+            fontSize: 16,
+            fontFamily: "Inter",
+            fontWeight: "500",
+            wordWrap: "break-word",
+            }}
+        >
+            Patrick Nelson
+        </div>
+        <div
+            className="Frame143"
+            style={{
+            paddingLeft: 15,
+            paddingRight: 15,
+            paddingTop: 8,
+            paddingBottom: 8,
+            left: 211,
+            top: 12,
+            position: "absolute",
+            background: "#F6E8BD",
+            borderRadius: 10,
+            overflow: "hidden",
+            border: "1px solid",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: 10,
+            display: "inline-flex",
+            }}
+        >
+            <div
+            className="AddContact"
+            style={{
+                color: "#1A1A1A",
+                fontSize: 14,
+                fontFamily: "Roboto",
+                fontWeight: "400",
+                wordWrap: "break-word",
+            }}
+            >
+            Add Contact
+            </div>
+        </div>
+        </div>
+    </div>
+    <div
+        className="Frame149"
+        style={{
+        width: 330,
+        height: 300,
+        left: 30,
+        top: 405,
+        position: "absolute",
+        background: "#F6E8BD",
+        borderRadius: 10,
+        overflow: "hidden",
+        }}
+    >
+        <div
+        className="Frame146"
+        style={{
+            width: 100,
+            height: 100,
+            left: 115,
+            top: 100,
+            position: "absolute",
+            background: "black",
+            borderRadius: 8,
+            overflow: "hidden",
+        }}
+        >
+        <div
+            className="T"
+            style={{
+            left: 37,
+            top: 26,
+            position: "absolute",
+            color: "white",
+            fontSize: 40,
+            fontFamily: "Inter",
+            fontWeight: "500",
+            wordWrap: "break-word",
+            }}
+        >
+            T
+        </div>
+        </div>
+    </div>
+    <div
+        className="Frame148"
+        style={{
+        width: 330,
+        height: 65,
+        left: 30,
+        top: 740,
+        position: "absolute",
+        background: "#1A1A1A",
+        borderRadius: 15,
+        overflow: "hidden",
+        }}
+    >
+        <div
+        className="Component9"
+        style={{ width: 49, height: 49, left: 141, top: 8, position: "absolute" }}
+        >
+        <div
+            className="Ellipse21"
+            style={{
+            width: 49,
+            height: 49,
+            left: 0,
+            top: 0,
+            position: "absolute",
+            background: "#5C5C5C",
+            borderRadius: 9999,
+            }}
+        />
+        <div
+            className="Icon"
+            style={{
+            width: 22.74,
+            height: 22.07,
+            left: 13,
+            top: 11,
+            position: "absolute",
+            }}
+        >
+        <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" viewBox="0 0 23 23" fill="none">
+        <path d="M11.8848 0C4.17777 0 -1.07308 7.80913 1.83475 14.9465L3.07787 17.9978C3.18911 18.2709 3.10779 18.5846 2.87792 18.7692L0.249262 20.8802C0.0286907 21.0573 -0.0560652 21.3544 0.0378263 21.6213C0.131717 21.8881 0.383808 22.0667 0.666704 22.0667H11.0937C17.5241 22.0667 22.7369 16.8539 22.7369 10.4235C22.7369 4.66678 18.0701 0 12.3134 0H11.8848Z" fill="#FAF9F6"/>
+        </svg>
+        </div>
+        </div>
+        <div
+        className="Component10"
+        style={{ width: 49, height: 49, left: 222, top: 8, position: "absolute" }}
+        >
+        <div
+            className="Ellipse21"
+            style={{
+            width: 49,
+            height: 49,
+            left: 0,
+            top: 0,
+            position: "absolute",
+            background: "#5C5C5C",
+            borderRadius: 9999,
+            }}
+        />
+        <div
+            className="SolidCommunicationPhoneMiss"
+            style={{ width: 32, height: 32, left: 9, top: 9, position: "absolute" }}
+        >
+        <div
+            className="Icon"
+            style={{
+            width: 23.33,
+            height: 25.71,
+            left: 0,
+            top: 0,
+            position: "absolute",
+            }}
+        >
+            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
+<path d="M24.701 12.5341C18.959 10.4057 12.6153 10.492 6.92213 12.7929L5.99593 13.1482C3.87692 13.961 2.64989 16.1804 3.08846 18.4072L3.49917 20.4925C3.63271 21.1704 4.26209 21.6341 4.94918 21.5606L10.0796 21.0118C10.8332 20.9312 11.3681 20.2395 11.2566 19.4899L10.9497 17.4261C14.3157 16.2839 17.9758 16.2839 21.3417 17.4261L21.0348 19.4899C20.9234 20.2395 21.4583 20.9312 22.2118 21.0118L27.3424 21.5606C28.0294 21.6341 28.6587 21.1706 28.7924 20.4928L29.2064 18.3936C29.6482 16.1534 28.4042 13.9225 26.2662 13.1209L24.7034 12.535L24.701 12.5341Z" fill="#FAF9F6"/>
+</svg>
+        </div>   
+        </div>
+        </div>
+        <div
+        className="Component8"
+        style={{ width: 49, height: 49, left: 60, top: 8, position: "absolute" }}
+        >
+        <div
+            className="Ellipse21"
+            style={{
+            width: 49,
+            height: 49,
+            left: 0,
+            top: 0,
+            position: "absolute",
+            background: "#5C5C5C",
+            borderRadius: 9999,
+            }}
+        />
+        <div
+            className="Icon"
+            style={{
+            width: 23.33,
+            height: 25.71,
+            left: 12,
+            top: 11,
+            position: "absolute",
+            }}
+        >
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="26" viewBox="0 0 24 26" fill="none">
+        <path d="M15.1059 1.92245C15.257 2.83941 15.3698 3.76109 15.4441 4.68509C15.4519 4.78197 15.4165 4.87727 15.3478 4.94599L7.32788 12.9659C7.17722 13.1166 6.92867 13.0371 6.89402 12.8269C6.299 9.21644 6.299 5.53291 6.89402 1.92245L6.96632 1.48379C7.06454 0.88777 7.52021 0.414089 8.11197 0.292835C10.0175 -0.0976118 11.9824 -0.0976118 13.8879 0.292835C14.4797 0.414089 14.9354 0.887769 15.0336 1.48379L15.1059 1.92245Z" fill="#FAF9F6"/>
+        <path d="M3.25742 14.8068C3.32269 15.2674 3.50859 15.6877 3.78219 16.0351C3.89714 16.181 3.90049 16.3933 3.76913 16.5247L2.82761 17.4662C2.6981 17.5957 2.48734 17.5972 2.36651 17.4596C1.79464 16.8081 1.40531 15.9915 1.2772 15.0874L0.676521 10.8483C0.599036 10.3015 0.979509 9.79539 1.52633 9.7179C2.07315 9.64042 2.57925 10.0209 2.65674 10.5677L3.25742 14.8068Z" fill="#FAF9F6"/>
+        <path d="M6.40833 19.1139L1.70711 23.8151C1.31658 24.2056 0.683417 24.2056 0.292893 23.8151C-0.0976311 23.4246 -0.0976311 22.7914 0.292893 22.4009L21.6262 1.06755C22.0168 0.677027 22.6499 0.677027 23.0404 1.06755C23.431 1.45808 23.431 2.09124 23.0404 2.48177L15.4429 10.0793C15.3685 10.9983 15.2562 11.9149 15.1059 12.8269L15.0336 13.2656C14.9354 13.8616 14.4797 14.3353 13.8879 14.4565C12.8612 14.6669 11.8173 14.7639 10.7746 14.7476L8.2626 17.2596C11.0168 17.4328 13.7833 17.3444 16.5249 16.9942C17.6723 16.8476 18.5802 15.9521 18.7425 14.8068L19.3432 10.5677C19.4207 10.0209 19.9268 9.64042 20.4736 9.7179C21.0204 9.79539 21.4009 10.3015 21.3234 10.8483L20.7227 15.0874C20.4341 17.1245 18.8192 18.7174 16.7783 18.9781C15.1913 19.1808 13.5964 19.2996 12 19.3346V24.708C12 25.2603 11.5523 25.708 11 25.708C10.4477 25.708 10 25.2603 10 24.708V19.3346C8.80106 19.3083 7.60291 19.2348 6.40833 19.1139Z" fill="#FAF9F6"/>
+        </svg>
+        </div>
+        </div>
+    </div>
+    </div>
+    )
+}
+
+export default VoiceChat
